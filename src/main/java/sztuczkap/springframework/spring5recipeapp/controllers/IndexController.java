0d5df3f773cda61f1a0e352fd8a3854069eb018d
sztuczkap <p.sztuczka@wp.pl>
2020-07-30 19:22:14 +0200

    Add index and index controller

--- src/main/java/sztuczkap/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/sztuczkap/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,4 +1,14 @@
 package sztuczkap.springframework.spring5recipeapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+
 }
